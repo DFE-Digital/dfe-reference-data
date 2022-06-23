3589521caef03d011e8e285eac7ace4bf250c9b7
Frankie Roberto <frankie@frankieroberto.com>
2022-06-23 14:32:30 +0100

    Add subject: English language and literature (#9)
    This is a common combined degree subject which has been entered 199 times as a UK degree subject.

--- lib/dfe/reference_data/degrees.rb
+++ lib/dfe/reference_data/degrees.rb
@@ -4990,6 +4990,12 @@ module DfE
             synonyms: [],
             dttp_id: 'e18070f0-5dce-e911-a985-000d3ab79618',
             hesa_itt_code: '100320' },
+          'dd967b4a-4d22-48a3-a2f8-d5719cee0519' =>
+          { name: 'English language and literature',
+            synonyms: [],
+            dttp_id: nil,
+            hesa_itt_code: nil,
+            comment: 'This is a common combined degree subject. Ideally it would map to 2 HESA codes, 100318 and 100319.' },
           '0b8670f0-5dce-e911-a985-000d3ab79618' =>
           { name: 'Enterprise and entrepreneurship',
             synonyms: [],
