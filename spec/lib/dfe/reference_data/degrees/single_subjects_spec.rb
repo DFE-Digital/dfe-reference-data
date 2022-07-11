1c7d9c71cb2fed1948470409391348552cde0486
Frankie Roberto <frankie@frankieroberto.com>
2022-07-11 10:04:59 +0100

    Add some basic tests for single subjects (#27)
    This just tries to catch us from making mistakes...

---
+++ spec/lib/dfe/reference_data/degrees/single_subjects_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe DfE::ReferenceData::Degrees::SINGLE_SUBJECTS do
+  describe 'single subjects' do
+    let(:single_subjects) { DfE::ReferenceData::Degrees::SINGLE_SUBJECTS.all }
+
+    it 'has a 6 digit HECoS code for each one or a null code' do
+      single_subjects.each do |subject|
+        expect(subject.hecos_code).to match(/\A\d{6}\z/).or be_nil
+      end
+    end
+
+    it 'has a unique name for each subject' do
+      single_subjects.each do |subject|
+        expect(subject.name).not_to be_nil
+        expect(subject.name).not_to be_blank
+      end
+
+      subjects_with_count = single_subjects.group_by(&:name)
+                                           .map { |subject_name, group| { name: subject_name, count: group.size } }
+
+      subjects_with_count.each do |subject|
+        expect(subject[:count]).to eql(1), "#{subject[:name]} is listed #{subject[:count]} times, should only be listed once"
+      end
+    end
+  end
+end
