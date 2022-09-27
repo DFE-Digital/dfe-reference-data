c7b42b4e8bc5b1cb9ee1e3f9f9a07a2816e07c49
Alaric Snell-Pym <alaric.snell-pym@digital.education.gov.uk>
2022-09-27 13:50:33 +0100

    Incorporated proposed combined mapping from FindPub

--- spec/lib/dfe/reference_data/itt/subjects_spec.rb
+++ spec/lib/dfe/reference_data/itt/subjects_spec.rb
@@ -1,19 +1,83 @@
 RSpec.describe DfE::ReferenceData::ITT::SUBJECTS do
   describe 'subjects' do
     let(:records) { described_class.all }
-    let(:allocation_subjects) { DfE::ReferenceData::ITT::ALLOCATION_SUBJECTS.all_as_hash }
+    let(:incentives) { DfE::ReferenceData::ITT::INCENTIVES.all_as_hash }
+    let(:publish_categories) { DfE::ReferenceData::ITT::PUBLISH_CATEGORIES.all_as_hash }
+    let(:register_categories) { DfE::ReferenceData::ITT::REGISTER_CATEGORIES.all_as_hash }
+    let(:categories) { DfE::ReferenceData::ITT::CATEGORIES.all_as_hash }
 
-    it 'has a valid allocation subject' do
+    it 'has a valid incentive' do
       expect(records).to be_all do |rec|
         name = rec.name
-        allocation_subject_id = rec.allocation_subject
-
-        unless allocation_subjects.key?(allocation_subject_id)
-          # Component ID isn't in SINGLE_SUBJECTS, definitely a problem
-          puts "Allocation subject ID mismatch in SUBJECTS[#{rec.id}] (#{name}): allocation subject ID #{allocation_subject_id} not found in ALLOCATION_SUBJECTS"
-          false
+        incentive_id = rec.incentive
+        if incentive_id
+          if incentives.key?(incentive_id)
+            true
+          else
+            puts "Incentive ID mismatch in SUBJECTS[#{rec.id}] (#{name}): incentive ID #{incentive_id} not found in INCENTIVES"
+            false
+          end
+        else
+          # No incentive, this is fine
+          true
         end
-        true
+      end
+    end
+
+    it 'has a valid publish_category' do
+      expect(records).to be_all do |rec|
+        name = rec.name
+        publish_category_id = rec.publish_category
+        if publish_category_id
+          if publish_categories.key?(publish_category_id)
+            true
+          else
+            puts "Publish category ID mismatch in SUBJECTS[#{rec.id}] (#{name}): publish_category ID #{publish_category_id} not found in PUBLISH_CATEGORIES"
+            false
+          end
+        else
+          # No publish_category, this is OK but issue a warning
+          puts "Missing publish_category in SUBJECTS[#{rec.id}] (#{name})"
+          true
+        end
+      end
+    end
+
+    it 'has a valid register category' do
+      expect(records).to be_all do |rec|
+        name = rec.name
+        register_category_id = rec.register_category
+        if register_category_id
+          if register_categories.key?(register_category_id)
+            true
+          else
+            puts "Register category ID mismatch in SUBJECTS[#{rec.id}] (#{name}): register category ID #{register_category_id} not found in REGISTER_CATEGORIES"
+            false
+          end
+        else
+          # No register_category, this is OK but issue a warning
+          puts "Missing register_category in SUBJECTS[#{rec.id}] (#{name})"
+          true
+        end
+      end
+    end
+
+    it 'has a valid category' do
+      expect(records).to be_all do |rec|
+        name = rec.name
+        category_id = rec.category
+        if category_id
+          if categories.key?(category_id)
+            true
+          else
+            puts "Category ID mismatch in SUBJECTS[#{rec.id}] (#{name}): category ID #{category_id} not found in CATEGORIES"
+            false
+          end
+        else
+          # No category, this is OK but issue a warning
+          puts "Missing category in SUBJECTS[#{rec.id}] (#{name})"
+          true
+        end
       end
     end
   end
