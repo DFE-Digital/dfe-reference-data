ed067729bc4914a4d6f5fae8a0722f63e16405d3
Alaric Snell-Pym <alaric.snell-pym@digital.education.gov.uk>
2022-11-07 17:50:34 +0000

    More field renamings

--- docs/lists_itt.md
+++ docs/lists_itt.md
@@ -23,6 +23,6 @@ Quality: Manually updated on an ad-hoc basis. Please submit a pull request if in
 | `apply_opens`       | datetime | The point at which Apply For Teacher Training starts allowing applications to courses in this cycle |
 | `apply_1_deadline`  | datetime | The point at which Apply For Teacher Training stops accepting apply 1 applications to courses in this cycle |
 | `apply_2_deadline`  | datetime | The point at which Apply For Teacher Training stops accepting apply 2 applications to courses in this cycle |
-| `end_of_cycle` | datetime | The point at which unresolved applications for courses in this cycle are rejected automatically |
+| `provider_decision_deadline` | datetime | The point at which unresolved applications for courses in this cycle are rejected automatically, so all provider application decisions must have been made |
 | `find_closes` | datetime | The point at which Find Postgraduate Teacher Training stops showing courses in this cycle |
-| `holidays` | map from symbol to date range | Holiday periods within this cycle (the symbols being names such as `christmas` and `easter` |
+| `non_working_days` | map from symbol to date range | Non-working-day periods within this cycle (the symbols being names such as `christmas` and `easter` |
--- lib/dfe/reference_data/itt/cycles.rb
+++ lib/dfe/reference_data/itt/cycles.rb
@@ -11,9 +11,9 @@ module DfE
         apply_opens: :datetime,
         apply_1_deadline: :datetime,
         apply_2_deadline: :datetime,
-        end_of_cycle: :datetime,
+        provider_decision_deadline: :datetime,
         find_closes: :datetime,
-        holidays: { kind: :map, key: :symbol, value: :daterange }
+        non_working_days: { kind: :map, key: :symbol, value: :daterange }
       }.freeze
 
       # Timezone used to specify dates+times in this file
@@ -30,27 +30,27 @@ module DfE
             apply_opens: make_local_time(2018, 10, 13, 9),
             apply_1_deadline: make_local_time(2019, 8, 24, 18),
             apply_2_deadline: make_local_time(2019, 9, 18, 18),
-            end_of_cycle: make_local_time(2021, 9, 29, 23, 59, 59),
+            provider_decision_deadline: make_local_time(2021, 9, 29, 23, 59, 59),
             find_closes: make_local_time(2019, 10, 3, 23, 59, 59),
-            holidays: {}
+            non_working_days: {}
           },
           '2019-2020' => {
             find_opens: make_local_time(2019, 10, 6, 9),
             apply_opens: make_local_time(2019, 10, 13, 9),
             apply_1_deadline: make_local_time(2020, 8, 24, 18),
             apply_2_deadline: make_local_time(2020, 9, 18, 18),
-            end_of_cycle: make_local_time(2021, 9, 29, 23, 59, 59),
+            provider_decision_deadline: make_local_time(2021, 9, 29, 23, 59, 59),
             find_closes: make_local_time(2020, 10, 3, 23, 59, 59),
-            holidays: {}
+            non_working_days: {}
           },
           '2020-2021' => {
             find_opens: make_local_time(2020, 10, 6, 9),
             apply_opens: make_local_time(2020, 10, 13, 9),
             apply_1_deadline: make_local_time(2021, 9, 7, 18),
             apply_2_deadline: make_local_time(2021, 9, 21, 18),
-            end_of_cycle: make_local_time(2021, 9, 29, 23, 59, 59),
+            provider_decision_deadline: make_local_time(2021, 9, 29, 23, 59, 59),
             find_closes: make_local_time(2021, 10, 4, 23, 59, 59),
-            holidays: {
+            non_working_days: {
               christmas: Date.new(2020, 12, 20)..Date.new(2021, 1, 1),
               easter: Date.new(2021, 4, 2)..Date.new(2021, 4, 16)
             }
@@ -60,9 +60,9 @@ module DfE
             apply_opens: make_local_time(2021, 10, 12, 9),
             apply_1_deadline: make_local_time(2022, 9, 6, 18), # 1st Tuesday of September
             apply_2_deadline: make_local_time(2022, 9, 20, 18), # 2 weeks after Apply 1 deadline
-            end_of_cycle: make_local_time(2022, 9, 28, 23, 59, 59), # 1 week and a day after Apply 2 deadline
+            provider_decision_deadline: make_local_time(2022, 9, 28, 23, 59, 59), # 1 week and a day after Apply 2 deadline
             find_closes: make_local_time(2022, 10, 3, 23, 59, 59), # The evening before the find opens in the new cycle
-            holidays: {
+            non_working_days: {
               christmas: Date.new(2021, 12, 14)..Date.new(2022, 1, 16),
               easter: Date.new(2022, 4, 4)..Date.new(2022, 4, 18)
             }
@@ -72,9 +72,9 @@ module DfE
             apply_opens: make_local_time(2022, 10, 11, 9), # Second Tuesday of October
             apply_1_deadline: make_local_time(2023, 9, 5, 18), # 1st Tuesday of September
             apply_2_deadline: make_local_time(2023, 9, 19, 18), # 2 weeks after Apply 1 deadline
-            end_of_cycle: make_local_time(2023, 9, 27, 23, 59, 59), # 1 week and a day after Apply 2 deadline
+            provider_decision_deadline: make_local_time(2023, 9, 27, 23, 59, 59), # 1 week and a day after Apply 2 deadline
             find_closes: make_local_time(2023, 10, 2, 23, 59, 59), # The evening before the find opens in the new cycle
-            holidays: {
+            non_working_days: {
               christmas: Date.new(2022, 12, 19)..Date.new(2023, 1, 6),
               easter: Date.new(2023, 3, 27)..Date.new(2023, 4, 10)
             }
@@ -84,9 +84,9 @@ module DfE
             apply_opens: make_local_time(2023, 10, 10, 9), # Second Tuesday of October
             apply_1_deadline: make_local_time(2024, 9, 3, 18), # 1st Tuesday of September
             apply_2_deadline: make_local_time(2024, 9, 17, 18), # 2 weeks after Apply 1 deadline
-            end_of_cycle: make_local_time(2024, 9, 25, 23, 59, 59), # 1 week and a day after Apply 2 deadline
+            provider_decision_deadline: make_local_time(2024, 9, 25, 23, 59, 59), # 1 week and a day after Apply 2 deadline
             find_closes: make_local_time(2024, 9, 30, 23, 59, 59), # The evening before the find opens in the new cycle
-            holidays: {
+            non_working_days: {
               christmas: Date.new(2023, 12, 18)..Date.new(2024, 1, 5),
               easter: Date.new(2024, 3, 18)..Date.new(2024, 4, 1)
             }
