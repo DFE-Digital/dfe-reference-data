aa021afddf2a4e40bff3d3de360f899f667ac570
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2022-11-24 15:52:48 +0000

    Add HESA domicile/country/postcode data

---
+++ spec/lib/dfe/reference_data/hesa/domiciles_spec.rb
@@ -0,0 +1,38 @@
+require 'dfe/reference_data/hesa/domiciles'
+
+RSpec.describe DfE::ReferenceData::HESA::Domiciles do
+  describe described_class::COUNTRIES_AND_TERRITORIES do
+    it_should_behave_like 'a list of valid records'
+
+    it 'contains normal mappings from ISO codes to country names' do
+      expect(described_class.one('AW').name).to eq('Aruba')
+      expect(described_class.one('UM-81').name).to eq('Baker Island')
+    end
+
+    it 'provides some HESA-specific mappings' do
+      expect(described_class.one('XC').name).to eq('Cyprus')
+      expect(described_class.one('XG').name).to eq('Northern Ireland')
+      expect(described_class.one('ZZ')).to be_nil
+    end
+  end
+
+  describe described_class::SPECIAL_ISO_CODES do
+    it_should_behave_like 'a list of valid records'
+
+    it 'provides some HESA-specific mappings' do
+      expect(described_class.one('AQ').code).to eq('XX')
+      expect(described_class.one(nil).code).to eq('ZZ')
+    end
+  end
+
+  describe described_class::CODES_FOR_UK_AND_CI do
+    it_should_behave_like 'a list of valid records'
+
+    it 'provides some HESA-specific mappings' do
+      expect(described_class.one(nil).code).to eq('ZZ')
+      expect(described_class.one('England').code).to eq('XF')
+      expect(described_class.one('Wales').code).to eq('XI')
+      expect(described_class.one('other').code).to eq('XK')
+    end
+  end
+end
