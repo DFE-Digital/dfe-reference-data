5b8ef51afb93b2c843ab8faf6288cfa87204ae9c
Erica Porter <ericaporter@users.noreply.github.com>
2023-12-11 13:28:15 +0000

    Add 2022 to dates

--- lib/dfe/reference_data/bank_holidays.rb
+++ lib/dfe/reference_data/bank_holidays.rb
@@ -18,6 +18,48 @@ module DfE
       # N.B. England and Wales only
       BANK_HOLIDAYS = DfE::ReferenceData::HardcodedReferenceList.new(
         {
+          '3a7c3c1d-7d6d-475d-af54-7989cf294d6e' => {
+            title: 'New Year’s Day',
+            date: DateTime.new(2022, 1, 3),
+            notes: 'Substitute day'
+          },
+          '6c1f10f3-c36c-4dec-8f88-3d224cf100fd' => {
+            title: 'Good Friday',
+            date: DateTime.new(2022, 4, 15)
+          },
+          '0b7dbaf9-3591-4e01-8fb0-b2fe4172ae6e' => {
+            title: 'Easter Monday',
+            date: DateTime.new(2022, 4, 18)
+          },
+          'cde43aa2-4b07-4dbf-95d2-9bc59bfbcb8f' => {
+            title: 'Early May bank holiday',
+            date: DateTime.new(2022, 5, 2),
+          },
+          'eb424558-a348-4778-997f-9475b1efebef' => {
+            title: 'Spring bank holiday',
+            date: DateTime.new(2022, 6, 2)
+          },
+          '8042b1af-cc29-4645-9f36-62bb363926f6' => {
+            title: 'Platinum Jubilee bank holiday',
+            date: DateTime.new(2022, 6, 3)
+          },
+          '8fa6c06b-996e-4a5b-8c62-141c187c5d31' => {
+            title: 'Summer bank holiday',
+            date: DateTime.new(2022, 8, 29)
+          },
+          '9dd1cea1-dab6-4ec1-9dc4-1f5ab3865040' => {
+            title: 'Bank Holiday for the State Funeral of Queen Elizabeth II',
+            date: DateTime.new(2022, 9, 19)
+          },
+          '6c1bce30-8c47-4ff6-9ec6-dd079f83b7eb' => {
+            title: 'Boxing Day',
+            date: DateTime.new(2022, 12, 26)
+          },
+          '28173c6f-fe05-4d02-a79b-299bcbab1638' => {
+            title: 'Christmas Day',
+            date: DateTime.new(2022, 12, 27),
+            notes: 'Substitute day'
+          },
           'd564f857-6e6a-4814-b86c-a55a7040dedc' => {
             title: 'New Year’s Day',
             date: DateTime.new(2023, 1, 2),
