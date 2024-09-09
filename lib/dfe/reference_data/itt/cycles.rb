ec17fcac1a040421e52cb4bbe5e0b0909f8e6d36
Erica Porter <ericaporter@users.noreply.github.com>
2024-09-09 10:40:44 +0100

    Remove full stops

--- lib/dfe/reference_data/itt/cycles.rb
+++ lib/dfe/reference_data/itt/cycles.rb
@@ -106,7 +106,7 @@ module DfE
             find_opens: make_local_time(2024, 10, 1, 9), # First Tuesday of October
             apply_opens: make_local_time(2024, 10, 8, 9), # Second Tuesday of October
             apply_1_deadline: nil, # This deadline is not applicable for this cycle
-            apply_2_deadline: make_local_time(2025, 9, 16, 18), # 3rd Tuesday in September. This is the final and only deadline to send ITT applications in the cycle.
+            apply_2_deadline: make_local_time(2025, 9, 16, 18), # 3rd Tuesday in September. This is the final and only deadline to send ITT applications in the cycle
             provider_decision_deadline: make_local_time(2025, 9, 24, 23, 59, 59), # 1 week and a day after Apply 2 deadline
             find_closes: make_local_time(2025, 10, 6, 23, 59, 59), # The evening before find opens in the new cycle
             non_working_days: {
@@ -118,7 +118,7 @@ module DfE
             find_opens: make_local_time(2025, 10, 7, 9), # First Tuesday of October
             apply_opens: make_local_time(2024, 10, 14, 9), # Second Tuesday of October
             apply_1_deadline: nil, # This deadline is not applicable for this cycle
-            apply_2_deadline: make_local_time(2026, 9, 22, 18), # 3rd Tuesday in September. This is the final and only deadline to send ITT applications in the cycle.
+            apply_2_deadline: make_local_time(2026, 9, 22, 18), # 3rd Tuesday in September. This is the final and only deadline to send ITT applications in the cycle
             provider_decision_deadline: make_local_time(2026, 9, 30, 23, 59, 59), # 1 week and a day after Apply 2 deadline
             find_closes: make_local_time(2026, 10, 5, 23, 59, 59), # The evening before find opens in the new cycle
             non_working_days: {
