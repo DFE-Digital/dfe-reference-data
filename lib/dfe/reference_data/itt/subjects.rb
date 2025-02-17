d378ed292f85493ec62bd9c8f7432f4120bd7a19
Max Mills <8balldigitalsolutions@gmail.com>
2025-02-17 17:18:17 +0000

    update graphic design

--- lib/dfe/reference_data/itt/subjects.rb
+++ lib/dfe/reference_data/itt/subjects.rb
@@ -783,7 +783,8 @@ module DfE
                                                       publish_category: 'W1',
                                                       incentive: {
                                                         '2020-2021' => 'd4024ca2-da1a-47ff-b497-62fed8fa5249',
-                                                        '2024-2025' => 'a324ae47-d9db-4eb5-9039-c767b8ec4f08'
+                                                        '2024-2025' => '65585baf-049f-4407-b1b6-9a01d597b7fd',
+                                                        '2025-2026' => 'aba9c49c-3b43-4f47-a903-26cd4b35e8d2'
                                                       },
                                                       category: '69b468df-f7ca-40cb-9410-ed5f4e5bd61b' },
           '05044321-4c35-45d5-bb37-42be71be6b80' => { name: 'applied biology',
